3bc812511a81ebcada3af2dc37a511fdb0c1ba52
mikeycody5 <mikeycody5@users.noreply.github.com>
2023-11-06 06:05:20 +0000

    deploy: b27a56d8449abdae98868ba2634d28404becf667

---
+++ precache-manifest.aa4df0edccde123c7934cfeeddf9dbf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f980ebc6dc67ef504462e919e97948",
+    "url": "/assignment-1-mikeycody5/index.html"
+  },
+  {
+    "revision": "e0f048cf752b21536761",
+    "url": "/assignment-1-mikeycody5/static/css/main.02971024.chunk.css"
+  },
+  {
+    "revision": "04352399eac1134dd8f9",
+    "url": "/assignment-1-mikeycody5/static/js/2.36ae5dac.chunk.js"
+  },
+  {
+    "revision": "1a71b02cc27b78778d7196930fa09b49",
+    "url": "/assignment-1-mikeycody5/static/js/2.36ae5dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0f048cf752b21536761",
+    "url": "/assignment-1-mikeycody5/static/js/main.cb457505.chunk.js"
+  },
+  {
+    "revision": "8c49f016db12bd0511a6",
+    "url": "/assignment-1-mikeycody5/static/js/runtime-main.fcc70001.js"
+  }
+]);
